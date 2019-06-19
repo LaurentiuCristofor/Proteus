7b5d2ac1d4b3c89e87d87dc665c3ccdb35f88e26
LaurentiuCristofor <laurentiu.cristofor@outlook.com>
2019-06-18 22:55:12 -0700

    Add method for obtaining assembly information.

---
+++ Proteus/Proteus/Common/ProteusInfo.cs
@@ -0,0 +1,18 @@
+﻿////////////////////////////////////////////////////////////////////////////////////////////////////
+/// (c) Laurentiu Cristofor
+/// This file is part of the Proteus Library and is made available under the MIT license.
+/// Do not use it if you have not received an associated LICENSE file.
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+using System.Reflection;
+
+namespace LaurentiuCristofor.Proteus.Common
+{
+    public abstract class ProteusInfo
+    {
+        public static AssemblyName GetAssemblyInfo()
+        {
+            return Assembly.GetExecutingAssembly().GetName();
+        }
+    }
+}
