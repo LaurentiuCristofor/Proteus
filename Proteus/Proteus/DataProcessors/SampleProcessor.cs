9715edf410d1a74b1596be820259aec256515db7
LaurentiuCristofor <laurentiu.cristofor@outlook.com>
2022-10-05 22:12:22 -0700

    Sort sample before writing it out

--- Proteus/Proteus/DataProcessors/SampleProcessor.cs
+++ Proteus/Proteus/DataProcessors/SampleProcessor.cs
@@ -5,10 +5,12 @@
 /// Do not use it if you have not received an associated LICENSE file.
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
+using System;
 using System.Collections.Generic;
 
 using LaurentiuCristofor.Proteus.Common;
 using LaurentiuCristofor.Proteus.Common.Random;
+using LaurentiuCristofor.Proteus.Common.Utilities;
 using LaurentiuCristofor.Proteus.DataProcessors.Parameters;
 using LaurentiuCristofor.Proteus.FileOperations;
 
@@ -27,9 +29,9 @@ namespace LaurentiuCristofor.Proteus.DataProcessors
         protected UnknownTotalSampler Sampler { get; set; }
 
         /// <summary>
-        /// Data structure used for holding the sample lines.
+        /// Data structure used for holding the sample lines along their line numbers.
         /// </summary>
-        protected List<string> SampleLines { get; set; }
+        protected List<Tuple<ulong, string>> SampleLinesWithNumbers { get; set; }
 
         public void Initialize(OutputIntegerParameters processingParameters)
         {
@@ -37,7 +39,7 @@ namespace LaurentiuCristofor.Proteus.DataProcessors
 
             this.Sampler = new UnknownTotalSampler(this.Parameters.IntegerValue);
 
-            this.SampleLines = new List<string>();
+            this.SampleLinesWithNumbers = new List<Tuple<ulong, string>>();
 
             this.OutputWriter = new FileWriter(this.Parameters.OutputFilePath, trackProgress: true);
         }
@@ -46,16 +48,17 @@ namespace LaurentiuCristofor.Proteus.DataProcessors
         {
             if (lineNumber <= (ulong)this.Parameters.IntegerValue)
             {
-                this.SampleLines.Add(line);
+                this.SampleLinesWithNumbers.Add(new Tuple<ulong, string>(lineNumber, line));
             }
             else
             {
-                // Check if the current line should be replace a sample line.
+                // Check if the current line should replace a sample line.
+                // Convert the number to an index for the replacement.
                 //
                 int sampleLineReplacementNumber = this.Sampler.EvaluateAnotherElement();
                 if (sampleLineReplacementNumber > 0)
                 {
-                    this.SampleLines[sampleLineReplacementNumber - 1] = line;
+                    this.SampleLinesWithNumbers[sampleLineReplacementNumber - 1] = new Tuple<ulong, string>(lineNumber, line);
                 }
             }
 
@@ -64,14 +67,18 @@ namespace LaurentiuCristofor.Proteus.DataProcessors
 
         public override void CompleteExecution()
         {
-            if (this.SampleLines.Count < this.Parameters.IntegerValue)
+            if (this.SampleLinesWithNumbers.Count < this.Parameters.IntegerValue)
             {
-                throw new ProteusException($"The input file is smaller than the requested sample size! The requested sample size was {this.Parameters.IntegerValue} but only {this.SampleLines.Count} lines were found.");
+                throw new ProteusException($"The input file is smaller than the requested sample size! The requested sample size was {this.Parameters.IntegerValue} but only {this.SampleLinesWithNumbers.Count} lines were found.");
             }
 
-            foreach (string line in this.SampleLines)
+            Timer timer = new Timer($"\n{Constants.Messages.SortingStart}", Constants.Messages.SortingEnd, countFinalLineEndings: 0);
+            this.SampleLinesWithNumbers.Sort();
+            timer.StopAndReport();
+
+            foreach (Tuple<ulong, string> tuple in this.SampleLinesWithNumbers)
             {
-                this.OutputWriter.WriteLine(line);
+                this.OutputWriter.WriteLine(tuple.Item2);
             }
 
             base.CompleteExecution();
