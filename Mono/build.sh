00c0a5f3a25d26f20604475d3e1f57c5bc5a571a
Laurentiu Cristofor <laurentiu.cristofor@outlook.com>
2022-10-22 12:30:46 -0700

    Explicitly enable Mono compiler optimizations

--- Mono/build.sh
+++ Mono/build.sh
@@ -6,8 +6,8 @@ mkdir -p build
 
 # Build Proteus.
 #
-mcs -target:library -out:./build/Proteus.dll -recurse:../Proteus/*.cs Proteus.AssemblyInfo.cs
+mcs -optimize+ -target:library -out:./build/Proteus.dll -recurse:../Proteus/*.cs Proteus.AssemblyInfo.cs
 
 # Build Cabeiro.
 #
-mcs -target:exe -out:./build/Cabeiro.exe -recurse:../Cabeiro/*.cs -reference:./build/Proteus.dll Cabeiro.AssemblyInfo.cs
+mcs -optimize+ -target:exe -out:./build/Cabeiro.exe -recurse:../Cabeiro/*.cs -reference:./build/Proteus.dll Cabeiro.AssemblyInfo.cs
